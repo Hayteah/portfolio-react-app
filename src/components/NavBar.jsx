a75703a42b5e77a84766a9e49fc203e111105802
Tunde <babatea4real@yahoo.com>
2023-11-09 16:51:01 +0100

    fix error

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -116,7 +116,7 @@ const NavBar = () => {
             <a
               className="flex justify-between items-center w-full text-gray-300"
               target="_blank"
-              href="https://drive.google.com/file/d/1is3ouIUVsFcWlCsowib55mx6nLWAtBMI/view?usp=sharing"
+              href="https://drive.google.com/file/d/19-js6eUA_Grw4DhUXDXKbOJryNbOmL7f/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
