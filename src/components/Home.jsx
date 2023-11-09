e43812cbe5c9797d6f4b8b79d1bbdf7c61d43f23
Tunde <babatea4real@yahoo.com>
2023-11-09 22:11:42 +0100

    final-fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -21,9 +22,10 @@ const Home = () => {
         </p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-800 hover:border-red-800">
-            View Work
+            <Link to="projects" smooth={true} duration={500}>
+              View Projects
+            </Link>
             <span className="group-hover:rotate-90 duration-300">
-              {" "}
               <HiArrowNarrowRight className="ml-3 " />
             </span>
           </button>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -53,7 +53,7 @@ const Projects = () => {
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Fitness App
+                Recipe App
               </span>
 
               <div className="pt-8 text-center">
