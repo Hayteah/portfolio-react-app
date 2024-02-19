2fe4b9ab7d1d410a2d2f11b1b869e010fd7d5e0f
Tunde <babatea4real@yahoo.com>
2024-02-19 19:21:18 +0100

    Add game arcade

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import fitnesApp from "../assets/GetFit-app.png";
 import recicpeChronicles from "../assets/recipe-chronicles.png";
+import gameArcade from "../assets/game-arcade.jpeg";
 
 const Projects = () => {
   return (
@@ -48,6 +49,33 @@ const Projects = () => {
           </div>
 
           <div
+            style={{ backgroundImage: `url(${gameArcade})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Game Arcade
+              </span>
+
+              <div className="pt-8 text-center">
+                <a target="_blank" href="https://game-arcade-five.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  target="_blank"
+                  href="https://github.com/Hayteah/game-arcade"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
             style={{ backgroundImage: `url(${recicpeChronicles})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
@@ -73,6 +101,7 @@ const Projects = () => {
               </div>
             </div>
           </div>
+        
         </div>
       </div>
     </div>
