21c922b1ea12f1364cde7ed1f172925172da6ce0
Tunde <babatea4real@yahoo.com>
2024-02-26 21:24:07 +0100

    change contact lower case to Uppercase

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 
 function App() {
